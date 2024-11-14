24918e4612d03916751bd4f0fb564a942f7d61b7
Matt Tsogt <mandah0628@gmail.com>
2024-11-14 13:56:58 -0600

    flexbox changes

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="heading">
+    <div className="flex items-center ">
       <h1>Herro? Herro?</h1>
     </div>
   );
