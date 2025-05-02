80935c146a13b72bee77f20034098370a7abfd6e
Matt Tsogt <mandah0628@gmail.com>
2025-05-01 23:00:15 -0600

    added route for updating user info in a protected group with the token validation middleware

--- server/src/routes/UserRoutes.go
+++ server/src/routes/UserRoutes.go
@@ -3,12 +3,16 @@ package routes
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/mandah0628/weatherapp/server/src/controller"
+	"github.com/mandah0628/weatherapp/server/src/middleware"
 )
 
 func UserRoutes(router *gin.Engine) {
 	userGroup := router.Group("/user")
 	{
 		userGroup.POST("/register", controller.RegisterUser)
-		userGroup.POST("login", controller.LoginUser)
+		userGroup.POST("/login", controller.LoginUser)
+
+		userGroup.Use(middleware.ValidateToken())
+		userGroup.PUT("/update", controller.UpdateUser)
 	}
 }
