93d3c6f68337dbc75a1ff2f284d9ff979223515a
Matt Tsogt <mandah0628@gmail.com>
2025-05-01 22:15:32 -0600

    finished controller for registration and added login

--- server/src/controller/userController.go
+++ server/src/controller/userController.go
@@ -1,11 +1,15 @@
 package controller
 
 import (
-	"github.com/gin-gonic/gin"
-	"github.com/mandah0628/weatherapp/server/src/utils"
 	"log"
+	"github.com/gin-gonic/gin"
+	"github.com/mandah0628/weatherapp/server/src/database"
+	"github.com/mandah0628/weatherapp/server/src/model"
+	"github.com/mandah0628/weatherapp/server/src/utils"
 )
 
+
+// registers user
 func RegisterUser(c *gin.Context) {
 	var requestBody struct {
 		Name     string `json:"name"`
@@ -23,6 +27,7 @@ func RegisterUser(c *gin.Context) {
 	// validate form
 	if requestBody.Email == "" || requestBody.Name == "" || requestBody.Password == "" {
 		c.JSON(400, gin.H{"error" : "Missing form field"})
+		return
 	}
 
 	// hash password
@@ -32,7 +37,78 @@ func RegisterUser(c *gin.Context) {
 		return
 	}
 
+	// build user model
+	user := model.User {
+		Name: requestBody.Name,
+		Email: requestBody.Email,
+		Password: hashedPassword,
+	}
+
+	// create user in db
+	if err := database.CreateUser(&user); err != nil {
+		c.JSON(500, gin.H{"error" : "Failed to create user"})
+		return
+	}
+
+	// generate token
+	userId := user.ID.String()
+	token, err := utils.GenerateToken(userId)
+	if err != nil {
+		c.JSON(500, gin.H{
+			"error" : "Error generating token",
+		})
+		return
+	}
+
+	log.Println("Returning toke, status 200", token)
 	c.JSON(200, gin.H{
-		"hashedPassword" : hashedPassword,
+		"token" : token,
+	})
+}
+
+
+
+func LoginUser(c *gin.Context) {
+	// build struct
+	var requestBody struct {
+		Email string `json:"email"`
+		Password string `json:"password"`
+	}
+
+	// bind json to struct
+	if err := c.ShouldBindJSON(&requestBody); err != nil {
+		log.Println("Error binding JSON")
+		c.JSON(400, gin.H{
+			"error" : "Bad request",
+		})
+		return
+	}
+
+	// find the user from the db
+	user, err := database.FindUserByEmail(requestBody.Email)
+	if err != nil {
+		c.JSON(401, gin.H{
+			"error" : "Incorrect email or password",
+		})
+		return;
+	}
+
+	if err := utils.VerifyPassword(user.Password, requestBody.Password); err != nil {
+		c.JSON(401, gin.H{
+			"error" : "Incorrect email or password",
+		})
+		return
+	}
+	
+	token, err := utils.GenerateToken(user.ID.String())
+	if err != nil {
+		c.JSON(500, gin.H{
+			"error" : "Internal server error",
+		})
+		return
+	}
+
+	c.JSON(200, gin.H{
+		"token" : token,
 	})
 }
