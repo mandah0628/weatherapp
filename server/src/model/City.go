f571ab0185c589adef9252bfed12f8199d4f7904
Matt Tsogt <mandah0628@gmail.com>
2025-05-02 15:45:45 -0600

    added City model and defined relationship with User model

---
+++ server/src/model/City.go
@@ -0,0 +1,27 @@
+package model
+
+import (
+	"time"
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
+
+type City struct{
+	ID uuid.UUID `gorm:"type:uuid;primaryKey"`
+	Name string `gorm:"not null;size:50"`
+	CountryName string `gorm:"not null;size:50"`
+	CountryCode string `gorm:"not null;size:2"`
+	Lat	float64 `gorm:"not null"`
+	Lon float64 `gorm:"not null"`
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	DeletedAt gorm.DeletedAt
+	UserID uuid.UUID `gorm:"type:uuid;not null"`
+}
+
+func (city *City) BeforeCreate(tx *gorm.DB) (err error) {
+	if city.ID == uuid.Nil {
+		city.ID = uuid.New()
+	}
+	return
+}
