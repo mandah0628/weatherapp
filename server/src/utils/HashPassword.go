0511451dcbd1d5dc63689c390bed6ef03170fc6a
Matt Tsogt <mandah0628@gmail.com>
2025-05-01 22:17:11 -0600

    removed comment

--- server/src/utils/HashPassword.go
+++ server/src/utils/HashPassword.go
@@ -4,14 +4,6 @@ import (
 	"golang.org/x/crypto/bcrypt"
 )
 
-// Hashes a password
-// 
-// Parameters:
-// 1) password: the unhashed password
-//
-// Returns:
-// If operations is successful, hashed password and nil.
-// If operations are unsuccessful, empty string and error
 func HashPassword(password string) (string, error) {
 	passwordBytes := []byte(password)
 	cost := bcrypt.DefaultCost
