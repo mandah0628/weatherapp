dd7f63a4fd889bbd65c0941ab79c44310a084c2d
Matt Tsogt <mandah0628@gmail.com>
2025-05-02 14:09:05 -0600

    changed var names for clarity and extracts token from cookie instead of auth header

--- server/src/middleware/ValidateToken.go
+++ server/src/middleware/ValidateToken.go
@@ -3,7 +3,6 @@ package middleware
 import (
 	"log"
 	"os"
-	"strings"
 	"github.com/gin-gonic/gin"
 	"github.com/golang-jwt/jwt/v5"
 )
@@ -11,39 +10,43 @@ import (
 func ValidateToken() gin.HandlerFunc {
 	return func(c *gin.Context) {
 
-		// get auth header
-		authHeader := c.GetHeader("Authorization")
-		tokenStr := strings.TrimPrefix(authHeader, "Bearer ")
-		if tokenStr == "" {
+		// get token from cookie
+		rawToken, err := c.Cookie("token")
+		if err != nil || rawToken == "" {
 			c.AbortWithStatusJSON(401, gin.H{
-				"error": "Missing or invalid Authorization header",
+				"error" : "Missing or invalid cookie",
 			})
 			return
 		}
 
-		secret := os.Getenv("JWT_SECRET")
 
-		token, err := jwt.Parse(tokenStr, func(token *jwt.Token) (interface{}, error) {
+		// verify token authenticity
+		secret := os.Getenv("JWT_SECRET")
+		decodedToken, err := jwt.Parse(rawToken, func(token *jwt.Token) (interface{}, error) {
 			return []byte(secret), nil
 		})
-		if err != nil || !token.Valid {
+		if err != nil || !decodedToken.Valid {
 			c.AbortWithStatusJSON(401, gin.H{
 				"error": "Invalid or expired token",
 			})
 			return
 		}
 
-		claims, ok := token.Claims.(jwt.MapClaims)
-		userId, ok2 := claims["userId"].(string)
-		if !ok || !ok2 {
+		// extract claims from decoded token
+		// then extract userId from claims
+		claims, claimsIsMap := decodedToken.Claims.(jwt.MapClaims)
+		userId, valueIsString := claims["userId"].(string)
+		if !claimsIsMap || !valueIsString {
 			c.AbortWithStatusJSON(401, gin.H{
 				"error": "Invalid token claims",
 			})
 			return
 		}
 
+		// store used id in context
 		c.Set("userId", userId)
 		log.Println("User id from token:", userId)
+
 		c.Next()
 	}
 }
