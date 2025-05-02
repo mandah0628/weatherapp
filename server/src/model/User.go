5a0a563c088c5d02fcec22c43a9cd6ae47231c7e
Matt Tsogt <mandah0628@gmail.com>
2025-05-01 22:16:07 -0600

    User model now uses uuid for the id

--- server/src/model/User.go
+++ server/src/model/User.go
@@ -1,12 +1,24 @@
 package model
 
 import (
+	"time"
+	"github.com/google/uuid"
 	"gorm.io/gorm"
 )
 
 type User struct {
-	gorm.Model
+	ID uuid.UUID `gorm:"type:uuid;primaryKey"`
 	Name string `gorm:"not null;size:50"`
 	Email string `gorm:"unique;not null;size:100"`
-	Password string `gorm:"not null;size:50"`
+	Password string `gorm:"not null;size:100"`
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	DeletedAt gorm.DeletedAt `gorm:"index"`
+}
+
+func (user *User) BeforeCreate(tx *gorm.DB) (err error) {
+	if user.ID == uuid.Nil {
+		user.ID = uuid.New()
+	}
+	return
 }
