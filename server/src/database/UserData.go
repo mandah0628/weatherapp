6dfa0277c5cb4a40a120f6616da1797cc07f1d9e
Matt Tsogt <mandah0628@gmail.com>
2025-05-01 22:15:54 -0600

    added db operations for creating a user and finding a user by their email

--- server/src/database/UserData.go
+++ server/src/database/UserData.go
@@ -1,1 +1,20 @@
 package database
+
+import (
+	"github.com/mandah0628/weatherapp/server/src/config"
+	"github.com/mandah0628/weatherapp/server/src/model"
+)
+
+func CreateUser(user *model.User) error {
+	return config.Postgres.Create(user).Error
+}
+
+func FindUserByEmail(email string) (*model.User, error) {
+	var user model.User
+	err := config.Postgres.Where("email = ?", email).First(&user).Error
+	if err != nil {
+		return nil, err
+	}
+
+	return &user, nil
+}
