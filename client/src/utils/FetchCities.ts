394c689454e14604705b9df64530f268ae5535c7
Matt Tsogt <mandah0628@gmail.com>
2025-05-03 12:21:02 -0600

    added type

--- client/src/utils/FetchCities.ts
+++ client/src/utils/FetchCities.ts
@@ -2,7 +2,7 @@ import { AxiosGeolocation } from "./Axios";
 
 const apiKey = process.env.OPENWEATHER_API_KEY;
 
-export default async function fetchCities(query, limit = 10) {
+export default async function fetchCities(query : string, limit : number = 10) {
   if (!query) return []; 
 
   try {
