703b9a88c429427b2f8f19b01e79b724cc91bad9
Matt Tsogt <mandah0628@gmail.com>
2025-05-04 01:10:45 -0600

    changed to server component

--- client/src/components/SearchSuggestions.tsx
+++ client/src/components/SearchSuggestions.tsx
@@ -1,15 +1,13 @@
-"use client";
-
-export default function SearchSuggestions({ items, onSelect }) {
+export default function SearchSuggestions({ citySuggestions, onSelect } : {citySuggestions : any, onSelect : any}) {
   return (
     <ul className="absolute left-0 right-0 bg-white border rounded-lg mt-2 max-h-60 overflow-y-auto">
-      {items.map((item, index) => (
+      {citySuggestions.map((city : any, index : any) => (
         <li
           key={index}
           className="p-2 hover:bg-gray-100 cursor-pointer"
-          onClick={() => onSelect(item)}
+          onClick={() => onSelect(city)}
         >
-          {item.name}, {item.state ? `${item.state}, ` : ""}{item.country}
+          {city.name}, {city.state ? `${city.state}, ` : ""}{city.country}
         </li>
       ))
       }
