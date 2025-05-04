6b9135052a14c782ead8febe84962b4ae31c3c81
Matt Tsogt <mandah0628@gmail.com>
2025-05-04 01:10:33 -0600

    added useEffect block to get browser location, and fetch weather data whenever coordinates change(city changes)

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,38 +1,64 @@
-"use client"
+"use client";
 
-import CurrentForecast from "@/components/WeatherInfo/CurrentForecast";
-import WeeklyForecast from "@/components/WeatherInfo/WeeklyForecast";
-import HourlyForecast from "@/components/WeatherInfo/HourlyForecast";
-import ExtraInfo from "@/components/WeatherInfo/ExtraInfo";
 import { useState, useEffect } from "react";
+import CurrentForecast from "@/components/WeatherInfo/CurrentForecast";
+import SearchBox from "@/components/SearchBox";
 import GetBrowserLocation from "@/utils/GetBrowserLocation";
 import FetchWeather from "@/utils/FetchWeather";
 
+export default function WeatherInfo() {
+  const [weatherData, setWeatherData] = useState<any>(null);
+  const [loading, setLoading] = useState(true);
+  const [weatherCoords, setWeatherCoords] = useState<any>(null);
 
-
-export default function WeatherInfo({})
-{
-  const [weatherData, setWeatherData] = useState({})
 
   useEffect(() => {
-    async function FetchWeatherData() {
+    (async () => {
       try {
-        const browserCoords : any = await GetBrowserLocation()
-        const res : any = await FetchWeather(browserCoords.lat, browserCoords?.lon)
-        setWeatherData(res.data)
-        console.log(res.data)
-      } catch (error) {
-        
+        const coords  = await GetBrowserLocation(); 
+        setWeatherCoords(coords);                  
+      } catch (err) {
+        console.error(err);
+        setLoading(false);
       }
-      
-    }
-
-    FetchWeatherData()
+    })();
   }, []);
-  
- 
-  return(
-    <>
-    </>
+
+
+
+  useEffect(() => {
+    if (!weatherCoords) {
+      return;  
+    }                  
+
+    (async () => {
+      try {
+        setLoading(true);
+        const res : any = await FetchWeather(weatherCoords.lat, weatherCoords.lon );
+        setWeatherData(res.data);
+        console.log(res.data)
+      } catch (err) {
+        console.error(err);
+      } finally {
+        setLoading(false);
+      }
+    })();                                          
+  }, [weatherCoords?.lat, weatherCoords?.lon]);
+
+  if (loading || !weatherData) return null;
+
+
+  return (
+    <div>
+      <SearchBox 
+        updateCoords={setWeatherCoords}
+      />
+
+      <CurrentForecast
+          currentWeather={{
+            ...weatherData.current, precipitation: weatherData.hourly[0].pop
+          }}
+      />
+    </div>
   );
 }
