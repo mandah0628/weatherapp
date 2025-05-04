055170daa213e295f2bf9950f45e330fb43468e6
Matt Tsogt <mandah0628@gmail.com>
2025-05-04 17:57:24 -0600

    built barebone structure and basic functionality displaying  weather info

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,22 +1,30 @@
 "use client";
 
 import { useState, useEffect } from "react";
-import CurrentForecast from "@/components/WeatherInfo/CurrentForecast";
-import SearchBox from "@/components/SearchBox";
 import GetBrowserLocation from "@/utils/GetBrowserLocation";
 import FetchWeather from "@/utils/FetchWeather";
+import SearchBox from "@/components/SearchBox";
+import CurrentForecast from "@/components/WeatherInfo/CurrentForecast";
+import HourlyForecast from "@/components/WeatherInfo/HourlyForecast";
+import WeeklyForecast from "@/components/WeatherInfo/WeeklyForecast";
+import ExtraInfo from "@/components/WeatherInfo/ExtraInfo";
+import GoogleMap from "@/components/WeatherInfo/GoogleMap";
+import FetchCity from "@/utils/FetchCity";
 
 export default function WeatherInfo() {
   const [weatherData, setWeatherData] = useState<any>(null);
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState<boolean>(true);
   const [weatherCoords, setWeatherCoords] = useState<any>(null);
-
 
   useEffect(() => {
     (async () => {
       try {
-        const coords  = await GetBrowserLocation(); 
-        setWeatherCoords(coords);                  
+        const browserCoords = await GetBrowserLocation();
+        const city = await FetchCity(browserCoords)
+        const cityCoords = {lat: city.lat, lon: city.lon}
+        console.log("Browser Coords:", browserCoords)
+        console.log("City info:", cityCoords, city.name)
+        setWeatherCoords(cityCoords);                  
       } catch (err) {
         console.error(err);
         setLoading(false);
@@ -36,7 +44,6 @@ export default function WeatherInfo() {
         setLoading(true);
         const res : any = await FetchWeather(weatherCoords.lat, weatherCoords.lon );
         setWeatherData(res.data);
-        console.log(res.data)
       } catch (err) {
         console.error(err);
       } finally {
@@ -59,6 +66,24 @@ export default function WeatherInfo() {
             ...weatherData.current, precipitation: weatherData.hourly[0].pop
           }}
       />
+
+      <GoogleMap
+        coords={{lat:weatherCoords.lat, lng:weatherCoords.lon}}
+      />
+
+      <HourlyForecast
+        data={weatherData.hourly}
+      />
+
+      <WeeklyForecast
+        data={weatherData.daily}
+      />
+
+      <ExtraInfo
+        data={weatherData.current}
+      />
+
+
     </div>
   );
 }
