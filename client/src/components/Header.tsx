cddcac424de3efe98ff10bca2b7271be35a40288
Matt Tsogt <mandah0628@gmail.com>
2025-05-07 22:39:50 -0600

    added basic header structure

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,7 +1,22 @@
-export default function Header() {
+import { Menu } from "lucide-react";
+import SearchBox from "./SearchBox";
+
+export default function Header({toggleSidebar, updateCoords} : any) {
     return(
-        <div>
-            
+        <div
+        className="relative flex items-center justify-center h-full"
+        >
+            <Menu
+                className="absolute left-4 cursor-pointer"
+                onClick={toggleSidebar}
+                size={35}
+            />
+
+            <div
+                className="w-85"
+            >
+                <SearchBox updateCoords={updateCoords}/>
+            </div>
         </div>
     )
 }
