fdfe097c03c8b47d735fd1a464a317b964baa5d3
Matt Tsogt <mandah0628@gmail.com>
2025-05-10 00:41:55 -0600

    added auth context provided to wrap app and added icon

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,9 +1,13 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { AuthProvider } from "@/context/AuthContext";
 
 export const metadata: Metadata = {
   title: "Weather App",
   description: "Weather app created with NextJS",
+  icons: {
+    icon: "/favicon.jpg"
+  }
 };
 
 export default function RootLayout({
@@ -14,7 +18,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        {children}
+        <AuthProvider>
+          {children}
+        </AuthProvider>
       </body>
     </html>
   );
