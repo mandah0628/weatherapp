a4fa6f10093ee6c6cc35b283e06a274c7171d51d
Matt Tsogt <mandah0628@gmail.com>
2025-05-13 21:52:33 -0600

    generates a random hash

---
+++ server/src/utils/GenerateVerificationToken.go
@@ -0,0 +1,15 @@
+package utils
+
+import (
+	"crypto/rand"
+	"encoding/hex"
+)
+
+func GenerateVerificationToken() (string,error) {
+	bytes := make([]byte, 32)
+	if _, err := rand.Read(bytes); err != nil {
+		return "", err
+	}
+
+	return hex.EncodeToString(bytes), nil
+}
