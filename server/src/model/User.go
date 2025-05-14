e2314bb2d544f0292fafa99d1ae5ffeab6cf6231
Matt Tsogt <mandah0628@gmail.com>
2025-05-13 23:03:30 -0600

    added more columns

--- server/src/model/User.go
+++ server/src/model/User.go
@@ -7,14 +7,16 @@ import (
 )
 
 type User struct {
-	ID uuid.UUID 				`gorm:"type:uuid;primaryKey"`
-	Name string 				`gorm:"not null;size:50"`
-	Email string 				`gorm:"unique;not null;size:100"`
-	Password string 			`gorm:"not null;size:100"`
-	CreatedAt time.Time
-	UpdatedAt time.Time
-	DeletedAt gorm.DeletedAt 	`gorm:"index"`
-	SavedCities []City 			`gorm:"foreignKey:UserID"`
+	ID uuid.UUID 				`gorm:"type:uuid;primaryKey" json:"id"`
+	Name string 				`gorm:"not null;size:50" json:"name"`
+	Email string 				`gorm:"unique;not null;size:100" json:"email"`
+	Password string 			`gorm:"not null;size:100" json:"password"`
+	Verified bool				`gorm:"default:false" json:"verified"`
+	VerificationToken string	`gorm:"size:255" json:"verificationToken"`
+	CreatedAt time.Time			`json:"createdAt"`
+	UpdatedAt time.Time			`json:"updatedAt"`
+	DeletedAt gorm.DeletedAt 	`gorm:"index" json:"deletedAt"`
+	SavedCities []City 			`gorm:"foreignKey:UserID" json:"savedCities"`
 }
 
 func (user *User) BeforeCreate(tx *gorm.DB) (err error) {
